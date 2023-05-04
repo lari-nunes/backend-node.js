48b3bce484bfce1108cd0423291bc6784412286a
lari-nunes <larissanunes0904@gmail.com>
2023-05-04 20:20:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function somar(num1, num2) {
+  return num1 + num2;
+}
+
+function subtrair(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+function par(num1) {
+  return num1 % 2 == 0;
+}
+
+console.log(somar(2, 5));
+console.log(subtrair(8, 5));
+console.log(multiplicar(5, 5));
+console.log(divisao(50, 2));
+console.log(par(4)); // true or false
